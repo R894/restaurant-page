47efc72a344478824731e81b02da0a8d258a8524
R894 <kamil.khalaileh@gmail.com>
2023-07-18 11:54:47 +0300

    update style

--- src/home.js
+++ src/home.js
@@ -3,6 +3,10 @@ import { createSection, createTitle } from './basePage.js';
 
 export function createHome(){
     let main = document.querySelector(".main");
+    let img = document.createElement("div");
+
+    img.classList.add('img-container');
+
 
     let title = createTitle('Pizza Haven');
     let section = createSection('Best Pizza in Legeor');
@@ -11,6 +15,7 @@ export function createHome(){
 
     main.appendChild(title);
     main.appendChild(section);
+    main.append(img);
     main.appendChild(section2);
     main.appendChild(section3);
 }
